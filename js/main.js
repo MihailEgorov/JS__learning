9d1f00511e0f735a5f078fbcc054c26a9033313c
EgorovMihail <61701018+EgorovMihail@users.noreply.github.com>
2020-10-11 11:34:28 +0300

    feat: created variables, printed a message in a modal window and in the console | main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let money = 300;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+
+alert('сообщение с любым текстом');
+
+console.log('Вывести в консоль сообщение с любым текстом))');
+console.log(`money = ${money}`);
+
+
