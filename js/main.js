04dd37acf28f7753208bcc4e81e89848d03b5f53
EgorovMihail <61701018+EgorovMihail@users.noreply.github.com>
2020-10-14 17:39:35 +0300

    fix: fixed the condition by which the yield status was assigned

--- js/main.js
+++ js/main.js
@@ -1,59 +1,43 @@
 "use strict";
 
-let expenses1, 
-expenses2, 
-amount1, 
-amount2, 
-budgetMonth, 
-mission1;
-
-
 let money = 10000;
-console.log( typeof(money) );
 let income = "нет";
-console.log( typeof(income) );
 let addExpenses = "Интернет, Такси, Коммуналка";
-console.log( addExpenses.length );
 let deposit = true;
-console.log( typeof(deposit) );
 let mission = 100000;
 let period = 12;
+
+console.log( typeof(money) );
+console.log( typeof(income) );
+console.log( typeof(deposit) );
+console.log( addExpenses.length );
 console.log( "\"Период равен" + " " + period + " " + "месяцев\"" );
 console.log( "Цель заработать" + " " + mission + " " + "рублей" );
-console.log( addExpenses.toLocaleLowerCase() );
-console.log( addExpenses.split(", ") );
+console.log( addExpenses.toLocaleLowerCase().split(", ") );
+
+
 
 let budgetDay = money / 30; 
-console.log('budgetDay: ', budgetDay);
-
-
 money = prompt("Ваш месячный доход?");
-console.log('money: ', money);
 addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
-console.log('addExpenses: ', addExpenses);
 deposit = confirm("Есть ли у вас депозит в банке?");
-console.log('deposit: ', deposit);
 
 
+let expenses1 = prompt("Введите обязательную статью расходов?");
+let amount1 = prompt("Во сколько это обойдется?");
+let expenses2 = prompt("Введите обязательную статью расходов?");
+let amount2 = prompt("Во сколько это обойдется?");
 
 
-expenses1 = prompt("Введите обязательную статью расходов?");
-console.log('expenses1: ', expenses1);
-amount1 = prompt("Во сколько это обойдется?");
-console.log('amount1: ', amount1);
-expenses2 = prompt("Введите обязательную статью расходов?");
-console.log('expenses2: ', expenses2);
-amount2 = prompt("Во сколько это обойдется?");
-console.log('amount2: ', amount2);
-
-budgetMonth = money - ( Number(amount1)  + Number(amount2) );
-console.log( "бюджет на месяц", budgetMonth );
-
-mission1 = Math.ceil(mission / budgetMonth);
-console.log('Цель будет достигнута через:' + " " + mission1 + " " + "месяцев");
-
+let budgetMonth = money - ( Number(amount1)  + Number(amount2) );
+let mission1 = Math.ceil(mission / budgetMonth);
 budgetDay = Math.floor(budgetMonth / 30);
+
+
+console.log( "бюджет на месяц", budgetMonth );
+console.log('Цель будет достигнута через:' + " " + mission1 + " " + "месяцев");
 console.log('Бюджеть на день: ', budgetDay);
+
 
 switch (true) {
     case budgetDay > 1200:
@@ -62,7 +46,7 @@ switch (true) {
     case (600 < budgetDay) && (budgetDay < 1200):
         console.log("У вас средний уровень дохода");
         break;
-    case 0 < budgetDay < 600:
+    case (0 < budgetDay) && (budgetDay < 600):
         console.log("К сожалению у вас уровень дохода ниже среднего");
         break;
     case budgetDay < 0:
