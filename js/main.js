eaf34f29665664cc6d2ceb11da79e440a94cec72
EgorovMihail <61701018+EgorovMihail@users.noreply.github.com>
2020-11-02 17:43:08 +0300

    feature: implemented the program via recursion

--- js/main.js
+++ js/main.js
@@ -2,77 +2,79 @@
 
 /*функция проверки на введение числа */
 let isNumber = function(n){
-
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
 
 let wraper = function(){
 
-  let max = 99,
-      min = 1,
-      iter,
-      num;
+  let iter,
+      num,
+      tmp = 1;
 
-  const getRandomInteger = function(min, max){
-
-    let random = Math.random() * (max - min) + min;
-    return Math.floor(random);
+/*Рандомим от 1 до 100 */
+  let getRandomInteger = function(){
+      return Math.floor( Math.random() * 100 + 1);
   };
-
-  let randomInteger = getRandomInteger(min, max);
+  let randomInteger = getRandomInteger();
   console.log('randomInteger: ', randomInteger);
 
   let start = function(){
 
-    num = prompt("Угадай число от 1 до 100");
-
-    if (!isNumber(num) && num !== null) {
-        alert("Введи число");
-    }
-  };
-
-
-  const startGame = function(){
-    
     alert("Давай сыграем в игру");
+      do {
+        iter = prompt("Сколько ты хочешь попыток?");
+      } while (!isNumber(iter));
 
-    do {
-      iter = prompt("Сколько ты хочешь попыток?");
-    } while (!isNumber(iter));
+      iter = Number(iter);
+      return iter;
+  };
+  start();
 
-    for (let i = 0; i < iter; i++) {
+  let getNum = function(){
 
-      start();
-      
-      switch (true) {
-        case num === null:
-          alert("Игра окончена!!!");
-          return;
-        case num < randomInteger:
-          alert("Загаданное число больше");
-          break;
-        case num > randomInteger:
-          alert("Загаданное число меньше");
-          break;
-        case +num === randomInteger:
-          alert("Поздравляю, Вы угадали!!!");
-          return;
-        case !isNumber(num) && num !== null:
-          continue;
-        
-        default: alert("Что то пошло не так(");
-          break;
-      }
-  
+    num = prompt("Угадай число от 1 до 100");
+    if (!isNumber(num) && num !== null) {
+      alert("Введи число");
     }
-
-    alert("Вы исчерпали свое колличество попыток");
   };
 
 
-return startGame();
+  let startGame = function(){
+
+    getNum();
+      
+    switch (true) {
+      case num === null:
+        alert("Игра окончена!!!");
+        return;
+      case num < randomInteger:
+        alert("Загаданное число больше");
+        break;
+      case num > randomInteger:
+        alert("Загаданное число меньше");
+        break;
+      case +num === randomInteger:
+        alert("Поздравляю, Вы угадали!!!");
+        return;
+      case !isNumber(num) && num !== null:
+         break; 
+      
+      default: alert("Что то пошло не так(");
+        break;
+    }
+
+    if (tmp !== iter) {
+      tmp++;
+      return startGame();
+    }
+    else{
+      return alert("Вы исчерпали свои попытки");
+    }
+  };
+
+  return startGame();
 };
 
-
 wraper();
+
