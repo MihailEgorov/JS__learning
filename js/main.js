dcb4af05051cb19e09099e4db50f0f042b9479d2
EgorovMihail <61701018+EgorovMihail@users.noreply.github.com>
2021-02-07 15:37:47 +0300

    Задание 10

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const collections = document.querySelectorAll(".books");
+const cardCollections = document.querySelectorAll(".book");
+const headerElems = document.getElementsByTagName("h2");
+const advertising = document.querySelector(".adv");
+const collectionsLi = document.getElementsByTagName("li");
+
+
+//console.log('collections: ', collections);
+
+
+
+collections[0].prepend(  
+                        cardCollections[1], 
+                        cardCollections[0],
+                        cardCollections[4],
+                        cardCollections[3],
+                        cardCollections[5]
+                    );
+
+document.body.style.backgroundImage = "url(../image/you-dont-know-js.jpg)";
+headerElems[2].innerHTML = "<a>Книга 3. this и Прототипы Объектов</a>";
+advertising.remove();
+
+
